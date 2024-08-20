8b136a13a5e9da66964620f797db633557459a0c
ViktoriiaYakaba <yakaba@bk.ru>
2024-08-20 22:34:22 +0200

    save sprite.svg

--- rentalCamper/src/components/equipment/Equipment.jsx
+++ rentalCamper/src/components/equipment/Equipment.jsx
@@ -1,24 +1,91 @@
-import { BsWind } from "react-icons/bs";
-import { TbAutomaticGearbox } from "react-icons/tb";
-import { TbToolsKitchen2 } from "react-icons/tb";
-import { PiTelevisionSimple } from "react-icons/pi";
-import { LuShowerHead } from "react-icons/lu";
+import React, { useState } from "react"; 
+import Icon from "../icon/icon";
 import css from './Equipment.module.css';
+import { PiWindLight } from "react-icons/pi";
+import clsx from "clsx";
 
 const Equipment = () => {
+  const [selectedEquipment, setSelectedEquipment] = useState({
+    AC: false,
+    Automatic: false,
+    Kitchen: false,
+    TV: false,
+    Shower: false,
+  });
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setSelectedEquipment((prevState) => ({
+      ...prevState,
+      [name]: checked,
+    }));
+  };
+
   return (
     <div className={css.container}>
-          <h3 className={css.title}>Vehicle equipment</h3>
-          <p className={css.text}>Filters</p>
-          <ul className={css.list}>
-            <li className={css.listItem}><BsWind size={32}/><p className={css.textList}>AC</p></li>
-            <li className={css.listItem}><TbAutomaticGearbox size={32} /><p className={css.textList}>Automatic</p></li>
-            <li className={css.listItem}><TbToolsKitchen2 size={32}/><p className={css.textList}>Kitchen</p></li>
-            <li className={css.listItem}><PiTelevisionSimple size={32} /><p className={css.textList}>TV</p></li>
-            <li className={css.listItem}><LuShowerHead size={32} /><p className={css.textList}>Shower/WC</p></li>
-          </ul>
+      <h3 className={css.title}>Vehicle equipment</h3>
+      <p className={css.text}>Filters</p>
+      <ul className={css.list}>
+        <li className={css.listItem}>
+          <input
+            type="checkbox"
+            name="AC"
+            checked={selectedEquipment.AC}
+            onChange={handleCheckboxChange}
+            className={css.checkbox} 
+          />
+          <PiWindLight size={32} />
+          <p className={css.textList}>AC</p>
+        </li>
+        <li className={css.listItem}>
+          <input
+            type="checkbox"
+            name="Automatic"
+            checked={selectedEquipment.Automatic}
+            onChange={handleCheckboxChange}
+            className={css.checkbox} 
+          />
+          <Icon width="32" height="32" icon="change" />
+          <p className={css.textList}>Automatic</p>
+        </li>
+        <li className={css.listItem}>
+          <input
+            type="checkbox"
+            name="Kitchen"
+            checked={selectedEquipment.Kitchen}
+            onChange={handleCheckboxChange}
+            className={css.checkbox} 
+          />
+          <Icon width="32" height="32" icon="kitchen" />
+          <p className={css.textList}>Kitchen</p>
+        </li>
+        <li className={css.listItem}>
+          <input
+            type="checkbox"
+            name="TV"
+            checked={selectedEquipment.TV}
+            onChange={handleCheckboxChange}
+            className={css.checkbox} 
+          />
+          <Icon width="32" height="32" icon="TV" />
+          <p className={css.textList}>TV</p>
+        </li>
+        <li className={css.listItem}>
+          <input
+            type="checkbox"
+            name="Shower"
+            checked={selectedEquipment.Shower}
+            onChange={handleCheckboxChange}
+            className={css.checkbox} 
+          />
+          <Icon width="32" height="32" icon="batch" />
+          <p className={css.textList}>Shower/WC</p>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Equipment
+export default Equipment;
+
+
--- rentalCamper/src/components/type/Type.jsx
+++ rentalCamper/src/components/type/Type.jsx
@@ -1,7 +1,5 @@
 import css from './Type.module.css';
-import VanIcon from '../../../images/svg/Button.svg';
-import FullIcon from '../../../images/svg/Button-2.svg';
-import AlcoveIcon from '../../../images/svg/Button-3.svg';
+import Icon from '../icon/icon';
 
 const Type = () => {
   return (
@@ -9,15 +7,15 @@ const Type = () => {
       <h3 className={css.title}>Vehicle type</h3>
       <ul className={css.list}>
         <li className={css.listItem}>
-          <img src={VanIcon} alt="Van icon" className={css.icon} />
+          <Icon width="32" height="32" icon="camper-1" />
           <p className={css.textList}>Van</p>
         </li>
         <li className={css.listItem}>
-          <img src={FullIcon} alt="Fully Integrated icon" className={css.icon} />
+          <Icon width="32" height="32" icon="camper-2" style={{ minWidth: '32px', minHeight: '32px' }} />
           <p className={css.textList}>Fully Integrated</p>
         </li>
         <li className={css.listItem}>
-          <img src={AlcoveIcon} alt="Alcove icon" className={css.icon} />
+          <Icon width="32" height="32" icon="camper-3" />
           <p className={css.textList}>Alcove</p>
         </li>
       </ul>
